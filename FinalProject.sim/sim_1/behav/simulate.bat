0d109299bb906d305e4cfa28c15df74e3cff4aae
Augustus Boling <geekish2428@gmail.com>
2017-06-10 21:01:54 -0700

    Deleted merging conflict files.

--- FinalProject.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Apps\\Xilinx\\Vivado\\2016.2\\bin
-call %xv_path%/xsim test_section1_behav -key {Behavioral:sim_1:Functional:test_section1} -tclbatch test_section1.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
