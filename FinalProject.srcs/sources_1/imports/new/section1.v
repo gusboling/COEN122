270556a332a35e9f8d587b2e374de4f06af89ea4
Augustus Boling <geekish2428@gmail.com>
2017-06-10 19:55:45 -0700

    Repaired section1.v after merge damage

---
+++ FinalProject.srcs/sources_1/imports/new/section1.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/05/2017 04:26:04 PM
+// Design Name: 
+// Module Name: section1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module section1(JUMP, BRANCH, SELECT, CLOCK, PC_OUT, INST);
+
+input CLOCK;
+input [1:0] SELECT;
+input [31:0] JUMP, BRANCH;
+
+output reg [31:0] PC_OUT;
+output reg [31:0] INST;
+
+reg [31:0] PC = 0;
+
+wire [31:0] INST_BUS;
+wire [31:0] PC_BUS;
+wire [31:0] ADD_BUS;
+wire [31:0] BUFF_BUS_PC;
+wire [31:0] BUFF_BUS_INST;
+
+mux3to1 PC_MUX(.clk(CLOCK), .a_in(ADD_BUS), .b_in(JUMP), .c_in(BRANCH), .select(SELECT), .out(PC_BUS));
+
+adder PC_ADDER(.A(PC_BUS), .clk(CLOCK), .out(ADD_BUS));
+
+InstructionMem INST_MEM(.address(PC_BUS), .clk(CLOCK), .instruction_out(INST_BUS));
+
+IF_ID buffer1(.clk(CLOCK), .PC4In(PC_BUS), .I_MemIn(INST_BUS), .PC4Out(BUFF_BUS_PC), .I_MemOut(BUFF_BUS_INST));
+
+always @ (PC_BUS)
+begin
+    PC = PC_BUS;
+    PC_OUT = BUFF_BUS_PC;
+    INST = BUFF_BUS_INST;
+end
+
+endmodule
