e709285fe10cbcc23c076130a9e6311c8aa8e631
Augustus Boling <geekish2428@gmail.com>
2017-06-10 21:08:25 -0700

    Removed conflicting merge files

--- FinalProject.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Apps\\Xilinx\\Vivado\\2016.2\\bin
-call %xv_path%/xsim test_section3_behav -key {Behavioral:sim_1:Functional:test_section3} -tclbatch test_section3.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
